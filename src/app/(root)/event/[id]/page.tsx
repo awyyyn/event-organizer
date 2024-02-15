4b3256aeabf8502824f049cd77c5b2a6056627d4
awyyyn <alwin.puche16@gmail.com>
2024-02-15 12:16:31 +0800

    add related events

--- src/app/(root)/event/[id]/page.tsx
+++ src/app/(root)/event/[id]/page.tsx
@@ -1,8 +1,9 @@
+import EventCard from "@/components/shared/event-card";
 import { Button } from "@/components/ui/button";
 import { EventResult } from "@/lib/types/extended";
 import { Event as EventType } from "@prisma/client";
 import Image from "next/image";
-import React from "react";
+import React, { Suspense } from "react";
 
 const origin =
 	process.env.NODE_ENV === "development"
@@ -13,15 +14,12 @@ async function getEvent(eventId: string) {
 	const result = await fetch(`${origin}/api/event/${eventId}`, {
 		method: "GET",
 	});
-
 	const event = await result.json();
-
 	return event as EventResult;
 }
 
 export default async function Event({ params }: { params: { id: string } }) {
 	const event = await getEvent(params.id);
-
 	const startDate = new Date(event.startDate).toDateString();
 	const endDate = new Date(event.endDate).toDateString();
 	const date = startDate === endDate ? startDate : `${startDate} - ${endDate}`;
@@ -30,7 +28,7 @@ export default async function Event({ params }: { params: { id: string } }) {
 		<div className="padding-x space-y-5 mt-5">
 			<h1 className="lg:text-5xl">{event.title}</h1>
 			<div className="flex flex-col lg:flex-row gap-5">
-				<div className="w-full shadow-md lg:w-8/12 relative h-[300px] lg:h-[500px]   ">
+				<div className="w-full shadow-md lg:w-7/12 relative h-[300px] lg:h-[500px]   ">
 					<Image
 						fill
 						alt={`${event.title} cover`}
@@ -38,9 +36,9 @@ export default async function Event({ params }: { params: { id: string } }) {
 						src={event.imageUrl}
 					/>
 				</div>
-				<div className=" space-y-2 w-full lg:w-4/12">
+				<div className=" space-y-2 w-full lg:w-5/12">
 					<div className="flex gap-2">
-						<h3>Location</h3>
+						<h3>Location:</h3>
 						<p className="event-data">
 							{event.location ? event.location : "unknown"}
 						</p>
@@ -81,15 +79,48 @@ export default async function Event({ params }: { params: { id: string } }) {
 						</p>
 					</div>
 
+					<div className="flex gap-2">
+						<h3>Description:</h3>
+						<p className="event-data">
+							{event.description ? event.description : "none"}
+						</p>
+					</div>
+
 					<Button>Buy ticket</Button>
 				</div>
 			</div>
-			<div>
-				<h3>Description:</h3>
-				<p className="event-data">
-					{event.description ? event.description : "none"}
-				</p>
-			</div>
+
+			<h3>Related Events</h3>
+			<Suspense fallback={<h1>Loading...</h1>}>
+				<RelatedEvents eventId={event.id} categoryId={event.categoryId} />
+			</Suspense>
+		</div>
+	);
+}
+
+async function getRelatedEvents(id: string) {
+	const result = await fetch(`${origin}/api/events?categoryId=${id}&take=9`, {
+		method: "GET",
+	});
+
+	const events = await result.json();
+	return events as EventResult[];
+}
+
+async function RelatedEvents({
+	categoryId,
+	eventId,
+}: {
+	eventId: string;
+	categoryId: string;
+}) {
+	const relatedEvents = await getRelatedEvents(categoryId);
+	const events = relatedEvents.filter((e) => e.id !== eventId);
+	return (
+		<div className="grid grid-cols-1 sm:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 grid-flow-dense gap-5">
+			{events.map((evnt) => (
+				<EventCard key={evnt.id} {...evnt} />
+			))}
 		</div>
 	);
 }
