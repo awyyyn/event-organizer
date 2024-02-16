787c1fe9afc6f2369f56d8653b663f93b0081fa8
awyyyn <alwin.puche16@gmail.com>
2024-02-16 11:04:15 +0800

    remove userId params

--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -5,6 +5,7 @@ export async function GET(req: NextRequest) {
 	const url = req.nextUrl;
 	const take = url.searchParams.get("take");
 	const category = url.searchParams.get("categoryId");
+	const userId = url.searchParams.get("userId");
 
 	try {
 		const events = await prisma.event.findMany({
@@ -14,9 +15,11 @@ export async function GET(req: NextRequest) {
 			},
 			where: {
 				categoryId: category ? category : undefined,
+				eventById: userId ? userId : undefined,
 			},
 			take: Number(take) === 0 ? undefined : Number(take),
 		});
+
 		return NextResponse.json(events, { status: 200, statusText: "OK" });
 	} catch (error) {
 		if (error instanceof Error) {
